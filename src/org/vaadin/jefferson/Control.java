13556b680ca319b6c39eb63a60e44fb3f3d50a18
Marlon Richert <marlon@vaadin.com>
2012-06-05 10:51:55 +0300

    #4: replaced "Listener" with "Controller", where appropriate

--- src/org/vaadin/jefferson/Control.java
+++ src/org/vaadin/jefferson/Control.java
@@ -1,64 +1,77 @@
+/*
+ * Copyright 2011, 2012 Vaadin Ltd.
+ * 
+ * Licensed under the GNU Affero General Public License, Version 3 (the 
+ * "License"); you may not use this file except in compliance with the License. 
+ * You may obtain a copy of the License at
+ * 
+ * http://www.gnu.org/licenses/agpl.html
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
 package org.vaadin.jefferson;
 
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
+import java.lang.reflect.*;
 
-import com.vaadin.ui.AbstractComponent;
-import com.vaadin.ui.Component;
+import com.vaadin.ui.*;
 import com.vaadin.ui.Field;
 
 /**
- * A {@link View} that can register a listener object to its rendition.
+ * A {@link View} that can register a controller object to its rendition.
  * 
  * @author Marlon Richert @ Vaadin
- * @param <L>
- *            The interface of this control's listener.
+ * @param <C>
+ *            The interface of this control's controller.
  */
-public abstract class Control<T extends Component, L> extends View<T> {
-    private L listener;
+public abstract class Control<P extends Component, C> extends View<P> {
+    private C controller;
     private Method removeListener;
     private Method addListener;
 
-    public Control(String name, Class<T> base, Class<L> listenerBase) {
+    public Control(String name, Class<P> base, Class<C> controllerBase) {
         super(name, base);
         try {
-            removeListener = base.getMethod("removeListener", listenerBase);
-            addListener = base.getMethod("addListener", listenerBase);
+            removeListener = base.getMethod("removeListener", controllerBase);
+            addListener = base.getMethod("addListener", controllerBase);
         } catch (NoSuchMethodException e) {
             throw new IllegalArgumentException(e);
         }
     }
 
-    public Control(String name, Class<T> base, Class<L> listenerBase, L listener) {
+    public Control(String name, Class<P> base, Class<C> listenerBase, C listener) {
         this(name, base, listenerBase);
-        setListener(listener);
+        setController(listener);
     }
 
-    public void setListener(L listener) {
-        T rendition = getRendition();
-        removeListener(rendition, this.listener);
+    public void setController(C listener) {
+        P rendition = getRendition();
+        removeListener(rendition, this.controller);
         addListener(rendition, listener);
-        this.listener = listener;
+        this.controller = listener;
     }
 
-    public L getListener() {
-        return listener;
+    public C getController() {
+        return controller;
     }
 
     @Override
-    protected T accept(Presentation presentation) {
-        T rendition = super.accept(presentation);
-        setListener(listener);
+    protected P accept(Presentation presentation) {
+        P rendition = super.accept(presentation);
+        setController(controller);
         return rendition;
     }
 
     @Override
-    protected boolean setRendition(T rendition) {
-        removeListener(getRendition(), listener);
+    protected boolean setRendition(P rendition) {
+        removeListener(getRendition(), controller);
         return super.setRendition(rendition);
     }
 
-    private void removeListener(T rendition, L handler) {
+    private void removeListener(P rendition, C handler) {
         try {
             if (rendition != null && handler != null) {
                 removeListener.invoke(rendition, handler);
@@ -70,7 +83,7 @@ public abstract class Control<T extends Component, L> extends View<T> {
         }
     }
 
-    private void addListener(T rendition, L handler) {
+    private void addListener(P rendition, C handler) {
         try {
             if (rendition != null) {
                 if (rendition instanceof AbstractComponent) {
